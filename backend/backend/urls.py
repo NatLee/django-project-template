92027e8163f5189b02aa25588e43c2f556bd0d94
Nat Lee <natlee.work@gmail.com>
2022-12-23 10:26:15 +0800

    [update] add http & https

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path
 from django.urls import include, re_path
+
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
-from django.contrib.auth.decorators import login_required
+from drf_yasg.generators import OpenAPISchemaGenerator
+
 from rest_framework.permissions import AllowAny, IsAdminUser
 
 from rest_framework.routers import DefaultRouter
@@ -44,7 +46,15 @@ urlpatterns += [
 ]
 
 
-# -------------- START - Swagger Setting --------------
+# -------------- START - Swagger View --------------
+
+# Http & Https
+class BothHttpAndHttpsSchemaGenerator(OpenAPISchemaGenerator):
+    def get_schema(self, request=None, public=False):
+        schema = super().get_schema(request, public)
+        schema.schemes = ["http", "https"]
+        return schema
+
 schema_view = get_schema_view(
     openapi.Info(
         title="Backend service API",
@@ -52,28 +62,29 @@ schema_view = get_schema_view(
         description="API of backend services.",
     ),
     public=True,
-    permission_classes=(AllowAny,)
-    # permission_classes = (IsAdminUser,) # is_staff才可使用
+    # permission_classes=(AllowAny,),
+    permission_classes = (IsAdminUser,), #is_staff才可使用
+    generator_class=BothHttpAndHttpsSchemaGenerator,
 )
+# --------------- END - Swagger View ----------------
 
-if settings.DEBUG:
-    urlpatterns += [
-        re_path(
-            r"^api/__hidden_swagger(?P<format>\.json|\.yaml)$",
-            schema_view.without_ui(cache_timeout=0),
-            name="schema-json",
-        ),
-        re_path(
-            r"^api/__hidden_swagger",
-            schema_view.with_ui("swagger", cache_timeout=0),
-            name="schema-swagger-ui",
-        ),
-        re_path(
-            r"^api/__hidden_redoc",
-            schema_view.with_ui("redoc", cache_timeout=0),
-            name="schema-redoc",
-        ),
-    ]
-# -------------- END - Swagger Setting --------------
+
+urlpatterns += [
+    re_path(
+        r"^api/__hidden_swagger(?P<format>\.json|\.yaml)$",
+        schema_view.without_ui(cache_timeout=0),
+        name="schema-json",
+    ),
+    re_path(
+        r"^api/__hidden_swagger",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
+    re_path(
+        r"^api/__hidden_redoc",
+        schema_view.with_ui("redoc", cache_timeout=0),
+        name="schema-redoc",
+    ),
+]
 
 
