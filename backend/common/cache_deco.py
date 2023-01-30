ae9abd81686a01a3fca3bc94624ecfedcd5d167e
Nat Lee <natlee.work@gmail.com>
2023-01-30 10:56:47 +0800

    [fix] typo

--- backend/common/cache_deco.py
+++ backend/common/cache_deco.py
@@ -7,7 +7,7 @@ class CacheDeco:
     def __init__(self):
         pass
     def __call__(self, func):
-        if settings.CACHE_PATG:
+        if settings.CACHE_PAGE:
             @method_decorator(cache_page(settings.CACHE_TTL))
             def wrapper(*args, **kwargs):
                 return func(*args, **kwargs)
