b5d408268e8e1af0ccd5f673ab8c752f48eff3ab
NatLee <natlee.work@gmail.com>
2024-07-12 03:38:52 +0800

    [update] add page of testing websocket

--- backend/ping/urls.py
+++ backend/ping/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 
 urlpatterns = [
     path("", views.Ping.as_view(), name="ping"),
+    path("/index", views.PingIndex, name="ping-index"),
     path("/job", views.PingJob.as_view(), name="ping-job"),
     path("/progress/<str:task_id>", views.PingJobProgress.as_view(), name="ping-progress"),
 ]
