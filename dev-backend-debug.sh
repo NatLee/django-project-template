06518a1f3a0962731c15f4eb67138e61ac7248bf
NatLee <natlee.work@gmail.com>
2024-07-30 23:37:09 +0800

    [update] rewrite debug script

--- dev-backend-debug.sh
+++ dev-backend-debug.sh
@@ -1,4 +1,28 @@
 #!/bin/bash
+
+# 載入環境變數
 export $(grep -v '^#' .env | xargs)
-docker exec -it $PROJECT_NAME-api bash -c "supervisorctl -c /etc/supervisor/conf.d/supervisord.conf stop django"
+
+# 檢查 supervisor 是否正在運行 Django
+supervisor_status=$(docker exec $PROJECT_NAME-api bash -c "supervisorctl -c /etc/supervisor/conf.d/supervisord.conf status django")
+
+if [[ $supervisor_status == *"RUNNING"* ]]; then
+echo "Supervisor 正在運行 Django，使用 supervisor 停止..."
+docker exec $PROJECT_NAME-api bash -c "supervisorctl -c /etc/supervisor/conf.d/supervisord.conf stop django"
+else
+echo "Supervisor 未運行 Django，嘗試查找是否有手動啟動的 Django ..."
+django_pid=$(docker exec $PROJECT_NAME-api bash -c "ps aux | grep 'python manage.py runserver' | grep -v grep | awk '{print \$2}'")
+
+if [ -n "$django_pid" ]; then
+    echo "找到手動啟動的 Django (PID: $django_pid)，正在停止..."
+    docker exec $PROJECT_NAME-api bash -c "kill $django_pid"
+else
+    echo "未找到運行中的 Django 。"
+fi
+fi
+
+# 等待幾秒鐘，確保舊的 Django 已經完全停止
+sleep 5
+
+echo "啟動新的 Django ..."
 docker exec -it $PROJECT_NAME-api bash -c "python manage.py runserver 0.0.0.0:8000"
