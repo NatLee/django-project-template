18462f3c788ccfefacd1e0da4566b564aed79f5e
Nat Lee <natlee.work@gmail.com>
2025-03-24 11:59:58 +0800

    [update] enhance dev-bash script to accept commands as arguments

--- dev-scripts/dev-bash.sh
+++ dev-scripts/dev-bash.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
 source "$(dirname "$0")/common.sh"
-
 print_message "$BLUE" "Executing command in ${CONTAINER_WEB_NAME}..."
-docker exec -it ${CONTAINER_WEB_NAME} bash
+
+# 如果有傳遞參數，則執行這些參數作為命令
+if [ $# -gt 0 ]; then
+    docker exec -it ${CONTAINER_WEB_NAME} "$@"
+else
+    # 如果沒有參數，則只是進入容器的 bash shell
+    docker exec -it ${CONTAINER_WEB_NAME} bash
+fi
